c250d759744bc6da90d3c599b4ce1ce07724d35c
shrima <shrima@shrima.(none)>
2013-03-08 21:01:01 +0530

    added 500

---
+++ 147/PeopleCircle.cpp
@@ -0,0 +1,195 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class PeopleCircle {
+public:
+	string order(int, int, int);
+};
+
+string PeopleCircle::order(int numMales, int numFemales, int k) {
+	string res="";
+	int pos=-1;
+	for(int i=0;i<numMales+numFemales;i++)
+		res+='M';
+	for(int i=0;i<numFemales;i++){
+		for(int c=0;c<k;){
+			pos++;
+			pos%=res.length();
+			if(res[pos]=='M')c++;
+		}
+		res[pos]='F';
+	}
+	return res;
+}
+
+
+double test0() {
+	int p0 = 5;
+	int p1 = 3;
+	int p2 = 2;
+	PeopleCircle * obj = new PeopleCircle();
+	clock_t start = clock();
+	string my_answer = obj->order(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "MFMFMFMM";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 7;
+	int p1 = 3;
+	int p2 = 1;
+	PeopleCircle * obj = new PeopleCircle();
+	clock_t start = clock();
+	string my_answer = obj->order(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "FFFMMMMMMM";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 25;
+	int p1 = 25;
+	int p2 = 1000;
+	PeopleCircle * obj = new PeopleCircle();
+	clock_t start = clock();
+	string my_answer = obj->order(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "MMMMMFFFFFFMFMFMMMFFMFFFFFFFFFMMMMMMMFFMFMMMFMFMMF";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 5;
+	int p1 = 5;
+	int p2 = 3;
+	PeopleCircle * obj = new PeopleCircle();
+	clock_t start = clock();
+	string my_answer = obj->order(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "MFFMMFFMFM";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int p0 = 1;
+	int p1 = 0;
+	int p2 = 245;
+	PeopleCircle * obj = new PeopleCircle();
+	clock_t start = clock();
+	string my_answer = obj->order(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "M";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
