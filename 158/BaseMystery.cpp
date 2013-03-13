e8ff96e1c18257cda1b77136d8aef5d383accafa
shrima <shrima@shrima.(none)>
2013-03-13 11:10:57 +0530

    added 500

---
+++ 158/BaseMystery.cpp
@@ -0,0 +1,443 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class BaseMystery {
+public:
+	vector <int> getBase(string);
+};
+
+map<char,int> m;
+
+int checkNum(string num,int base){
+	for(int i=0;i<num.length();i++){
+		if(m[num[i]]>=base)
+			return 0;
+	}
+	return 1;
+}
+
+int getNum(string num,int base){
+	int res=0;
+	for(int i=0;i<num.length();i++){
+		res*=base;
+		res+=m[num[i]];
+	}
+	return res;
+}
+
+int isBase(string e,int base){
+	int p=e.find('+');
+	e[p]=' ';
+	p=e.find('=');
+	e[p]=' ';
+	stringstream s(e);
+	string a1,b1,c1;
+	s>>a1>>b1>>c1;
+	int a,b,c;
+	if(checkNum(a1,base) && checkNum(b1,base) && checkNum(c1,base)){
+		int a=getNum(a1,base);
+		int b=getNum(b1,base);
+		int c=getNum(c1,base);
+		if(a+b==c){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+vector <int> BaseMystery::getBase(string equation) {
+	VI res;
+	for(int i=0;i<10;i++)
+		m[i+'0']=i;
+	int x=10;
+	for(char i='A';i<='J';i++,x++)
+		m[i]=x;
+	for(int i=1;i<=20;i++){
+		if(isBase(equation,i))
+			res.pb(i);
+	}
+	return res;
+
+}
+
+
+double test0() {
+	string p0 = "1+1=2";
+	BaseMystery * obj = new BaseMystery();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getBase(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 3,  4,  5,  6,  7,  8,  9,  10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "1+1=10";
+	BaseMystery * obj = new BaseMystery();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getBase(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 2 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "1+1=3";
+	BaseMystery * obj = new BaseMystery();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getBase(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	vector <int> p1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "ABCD+211=B000";
+	BaseMystery * obj = new BaseMystery();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getBase(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 14 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "ABCD+322=B000";
+	BaseMystery * obj = new BaseMystery();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getBase(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 15 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test5() {
+	string p0 = "1+0=1";
+	BaseMystery * obj = new BaseMystery();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getBase(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 2,  3,  4,  5,  6,  7,  8,  9,  10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test6() {
+	string p0 = "GHIJ+1111=HJ00";
+	BaseMystery * obj = new BaseMystery();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getBase(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 20 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test7() {
+	string p0 = "1234+8765=9999";
+	BaseMystery * obj = new BaseMystery();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getBase(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	time = test5();
+	if (time < 0)
+		errors = true;
+	
+	time = test6();
+	if (time < 0)
+		errors = true;
+	
+	time = test7();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
---
+++ 158/TireRotation.cpp
@@ -0,0 +1,201 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class TireRotation {
+public:
+	int getCycle(string, string);
+};
+
+int TireRotation::getCycle(string initial, string current) {
+	vector<string> s;
+	s.push_back(initial);
+	for(int i=1;i<4;i++){
+		string one=s[i-1];
+		one[2]=s[i-1][0];
+		one[3]=s[i-1][1];
+		one[0]=s[i-1][3];
+		one[1]=s[i-1][2];
+		s.push_back(one);
+	}
+	int res=-1;
+	for(int i=0;i<4;i++){
+		if(s[i]==current)
+			{res=i+1;break;}
+	}
+	return res;
+	
+}
+
+
+double test0() {
+	string p0 = "ABCD";
+	string p1 = "ABCD";
+	TireRotation * obj = new TireRotation();
+	clock_t start = clock();
+	int my_answer = obj->getCycle(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "ABCD";
+	string p1 = "DCAB";
+	TireRotation * obj = new TireRotation();
+	clock_t start = clock();
+	int my_answer = obj->getCycle(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "ABCD";
+	string p1 = "CDBA";
+	TireRotation * obj = new TireRotation();
+	clock_t start = clock();
+	int my_answer = obj->getCycle(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "ABCD";
+	string p1 = "ABDC";
+	TireRotation * obj = new TireRotation();
+	clock_t start = clock();
+	int my_answer = obj->getCycle(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = -1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "ZAXN";
+	string p1 = "XNAZ";
+	TireRotation * obj = new TireRotation();
+	clock_t start = clock();
+	int my_answer = obj->getCycle(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
