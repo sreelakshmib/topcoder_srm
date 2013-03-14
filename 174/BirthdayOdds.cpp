406fdc01539459c91da2045cd9161e31883dca8f
shrima <shrima@shrima.(none)>
2013-03-14 09:08:17 +0530

    added 500

---
+++ 174/BirthdayOdds.cpp
@@ -0,0 +1,170 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class BirthdayOdds {
+public:
+	int minPeople(int, int);
+};
+
+int BirthdayOdds::minPeople(int minOdds, int daysInYear) {
+	double req=1-minOdds*1.0/100;
+	double odd=1;
+	int cnt=0;
+	while(req<odd){
+		++cnt;
+		double d=1;
+		for(int i=daysInYear,j=0;j<cnt;i--,j++){
+			d*=i;
+			d/=daysInYear;
+		}
+		odd=d;
+	}
+	return cnt;
+}
+
+
+double test0() {
+	int p0 = 75;
+	int p1 = 5;
+	BirthdayOdds * obj = new BirthdayOdds();
+	clock_t start = clock();
+	int my_answer = obj->minPeople(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 50;
+	int p1 = 365;
+	BirthdayOdds * obj = new BirthdayOdds();
+	clock_t start = clock();
+	int my_answer = obj->minPeople(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 23;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 1;
+	int p1 = 365;
+	BirthdayOdds * obj = new BirthdayOdds();
+	clock_t start = clock();
+	int my_answer = obj->minPeople(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 84;
+	int p1 = 9227;
+	BirthdayOdds * obj = new BirthdayOdds();
+	clock_t start = clock();
+	int my_answer = obj->minPeople(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 184;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
---
+++ 174/CrossWord.cpp
@@ -0,0 +1,232 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class CrossWord {
+public:
+	int countWords(vector <string>, int);
+};
+
+int CrossWord::countWords(vector <string> board, int size) {
+	int res=0;
+	for(int i=0;i<board.size();i++){
+		int sz=0;
+		for(int j=0;j<board[0].size();j++){
+			if(board[i][j]=='.')sz++;
+			else{
+				if(sz==size)res++;
+				sz=0;
+			}
+		}
+		if(sz==size)res++;
+	}
+	return res;
+	
+}
+
+
+double test0() {
+	string t0[] = {"X....X",
+ "X.XX.X",
+ "...X..",
+ "X.XX.X",
+ "..X..."};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 3;
+	CrossWord * obj = new CrossWord();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"...X...",
+ ".X...X.",
+ "..X.X..",
+ "X..X..X",
+ "..X.X..",
+ ".X...X.",
+ "...X..."};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 3;
+	CrossWord * obj = new CrossWord();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 6;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string t0[] = {".....X....X....",
+ ".....X....X....",
+ "..........X....",
+ "....X....X.....",
+ "...X....X....XX",
+ "XXX...X....X...",
+ ".....X....X....",
+ ".......X.......",
+ "....X....X.....",
+ "...X....X...XXX",
+ "XX....X....X...",
+ ".....X....X....",
+ "....X..........",
+ "....X....X.....",
+ "....X....X....."}
+;
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 5;
+	CrossWord * obj = new CrossWord();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 8;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string t0[] = {"...",
+ "...",
+ "..."};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 50;
+	CrossWord * obj = new CrossWord();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string t0[] = {"....",
+ "....",
+ "...."};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 3;
+	CrossWord * obj = new CrossWord();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
