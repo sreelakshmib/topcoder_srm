4dd26e005918e9202dc1848938d01ee4c1934364
shrima <shrima@shrima.(none)>
2013-03-14 21:32:37 +0530

    added 500

---
+++ 176/Matching.cpp
@@ -0,0 +1,255 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class Matching {
+public:
+	vector <string> findMatch(vector <string>, vector <string>);
+};
+
+vector <string> Matching::findMatch(vector <string> first, vector <string> second) {
+	vector<string> res;
+	string state[4][3]={{"CIRCLE","SQUIGGLE","DIAMOND"},
+                        {"RED","GREEN","BLUE"},
+                        {"SOLID","STRIPED","EMPTY"},
+                        {"ONE","TWO","THREE"}         };
+	for(int i=0;i<4;i++){
+		int v=0;
+		for(int j=0;j<3;j++){
+			if(first[i]==state[i][j])v+=j;
+			if(second[i]==state[i][j])v+=j;
+		}
+		res.pb(state[i][(9-v)%3]);
+	}
+	return res;
+}
+
+
+double test0() {
+	string t0[] = {"DIAMOND","BLUE","SOLID","ONE"}
+;
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	string t1[] = {"DIAMOND","GREEN","SOLID","TWO"}
+;
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	Matching * obj = new Matching();
+	clock_t start = clock();
+	vector <string> my_answer = obj->findMatch(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t2[] = { "DIAMOND",  "RED",  "SOLID",  "THREE" };
+	vector <string> p2(t2, t2+sizeof(t2)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<"\""<<p2[0]<<"\"";
+		for (int i=1; i<p2.size(); i++)
+			cout <<", \"" <<p2[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"CIRCLE","GREEN","EMPTY","TWO"}
+;
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	string t1[] = {"DIAMOND","BLUE","STRIPED","ONE"}
+;
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	Matching * obj = new Matching();
+	clock_t start = clock();
+	vector <string> my_answer = obj->findMatch(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t2[] = { "SQUIGGLE",  "RED",  "SOLID",  "THREE" };
+	vector <string> p2(t2, t2+sizeof(t2)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<"\""<<p2[0]<<"\"";
+		for (int i=1; i<p2.size(); i++)
+			cout <<", \"" <<p2[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string t0[] = {"DIAMOND","RED","SOLID","ONE"}
+;
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	string t1[] = {"SQUIGGLE","BLUE","SOLID","TWO"}
+;
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	Matching * obj = new Matching();
+	clock_t start = clock();
+	vector <string> my_answer = obj->findMatch(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t2[] = { "CIRCLE",  "GREEN",  "SOLID",  "THREE" };
+	vector <string> p2(t2, t2+sizeof(t2)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<"\""<<p2[0]<<"\"";
+		for (int i=1; i<p2.size(); i++)
+			cout <<", \"" <<p2[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string t0[] = {"SQUIGGLE","RED","STRIPED","ONE"}
+;
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	string t1[] = {"SQUIGGLE","RED","STRIPED","ONE"}
+;
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	Matching * obj = new Matching();
+	clock_t start = clock();
+	vector <string> my_answer = obj->findMatch(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t2[] = { "SQUIGGLE",  "RED",  "STRIPED",  "ONE" };
+	vector <string> p2(t2, t2+sizeof(t2)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<"\""<<p2[0]<<"\"";
+		for (int i=1; i<p2.size(); i++)
+			cout <<", \"" <<p2[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
---
+++ 176/RGBColor.cpp
@@ -0,0 +1,240 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class RGBColor {
+public:
+	vector <int> getComplement(vector <int>);
+};
+
+vector <int> RGBColor::getComplement(vector <int> rgb) {
+	vector<int> res=rgb;
+	for(int i=0;i<rgb.size();i++){
+		res[i]=255-rgb[i];
+	}
+	if(abs(res[0]-rgb[0])<=32 && abs(res[1]-rgb[1])<=32 && abs(res[2]-rgb[2])<=32){
+		res[0]=rgb[0];		if(res[0]+128<=255)res[0]+=128;
+		else res[0]-=128;
+		res[1]=rgb[1];
+		if(res[1]+128<=255)res[1]+=128;
+		else res[1]-=128;
+		res[2]=rgb[2];
+		if(res[2]+128<=255)res[2]+=128;
+		else res[2]-=128;
+	}
+	return res;	
+}
+
+
+double test0() {
+	int t0[] = {255,0,0};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	RGBColor * obj = new RGBColor();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getComplement(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 0,  255,  255 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {115,115,143};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	RGBColor * obj = new RGBColor();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getComplement(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 243,  243,  15 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {115,115,144};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	RGBColor * obj = new RGBColor();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getComplement(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 140,  140,  111 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {153,12,55};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	RGBColor * obj = new RGBColor();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getComplement(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 102,  243,  200 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
