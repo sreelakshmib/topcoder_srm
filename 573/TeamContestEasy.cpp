a1e91326a0e913df3f2520585f9b23b24632d86d
shrima <shrima@shrima.(none)>
2013-03-15 09:15:22 +0530

    added 500

---
+++ 573/TeamContestEasy.cpp
@@ -0,0 +1,263 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class TeamContestEasy {
+public:
+	int worstRank(vector <int>);
+};
+
+int TeamContestEasy::worstRank(vector <int> s) {
+	int my=s[0]+s[1]+s[2]-min(s[0],min(s[1],s[2]));
+	s[0]=s[1]=s[2]=-1;
+	sort(s.begin(),s.end());
+	int rank=1;
+	int off=3;
+	a:
+	for(int i=off;i<s.size();i++){
+		for(int j=i+1;j<s.size();j++){
+			for(int k=j+1;k<s.size();k++){
+				int x=s[i]+s[j]+s[k]-min(s[i],min(s[j],s[k]));
+				if(x>my){
+					rank++;
+					s[i]=s[j]=s[k]=-1;
+					sort(s.begin(),s.end());
+					off+=3;
+					goto a;
+				}
+			}
+		}
+	}
+	return rank;	
+}
+
+
+double test0() {
+	int t0[] = {5, 7, 3, 5, 7, 3, 5, 7, 3};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	TeamContestEasy * obj = new TeamContestEasy();
+	clock_t start = clock();
+	int my_answer = obj->worstRank(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {5, 7, 3};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	TeamContestEasy * obj = new TeamContestEasy();
+	clock_t start = clock();
+	int my_answer = obj->worstRank(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	TeamContestEasy * obj = new TeamContestEasy();
+	clock_t start = clock();
+	int my_answer = obj->worstRank(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {2,2,1,1,3,1,3,2,1,3,1,2,1,2,1}
+;
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	TeamContestEasy * obj = new TeamContestEasy();
+	clock_t start = clock();
+	int my_answer = obj->worstRank(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int t0[] = {45,72,10,42,67,51,33,21,8,51,17,72}
+;
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	TeamContestEasy * obj = new TeamContestEasy();
+	clock_t start = clock();
+	int my_answer = obj->worstRank(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test5() {
+	int t0[] = {570466,958327,816467,17,403,953808,734850,5824,917784,921731,161921,1734,823437,3218,81,932681,2704,981643,1232,475,873323,6558,45660,1813,4714,224,
+32301,28081,6728,17055,561,15146,842613,5559,1860,783,989,2811,20664,112531,1933,866794,805528,53821,2465,137385,39,2007};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	TeamContestEasy * obj = new TeamContestEasy();
+	clock_t start = clock();
+	int my_answer = obj->worstRank(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 6;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test6() {
+	int t0[] = {610297,849870,523999,6557,976530,731458,7404,795100,147040,110947,159692,40785,4949,2903,1688,37278,620703,28156,16823,1159,12132,971379,5916,1159,988589,
+12215,133,1490,911360,920059,544070,40249,514852,852,745070,1105,715897,714696,589133,698317,5683,631612,16453,101000,764881,101,2053,754661};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	TeamContestEasy * obj = new TeamContestEasy();
+	clock_t start = clock();
+	int my_answer = obj->worstRank(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 10;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	time = test5();
+	if (time < 0)
+		errors = true;
+	
+	time = test6();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
