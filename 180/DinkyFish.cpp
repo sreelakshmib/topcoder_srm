56b559f2c7dbbfddcd76d9c81365a5e8ebbe5376
shrima <shrima@shrima.(none)>
2013-03-19 11:19:31 +0530

    added 250

---
+++ 180/DinkyFish.cpp
@@ -0,0 +1,228 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class DinkyFish {
+public:
+	int monthsUntilCrowded(int, int, int);
+};
+
+int DinkyFish::monthsUntilCrowded(int tankVolume, int maleNum, int femaleNum) {
+	int res=0;
+	int f=femaleNum,m=maleNum,t=tankVolume;
+	if(m+f>2*t)return 0;
+	while(m+f<=2*t){
+		res++;
+		int p=min(m,f);
+		m+=p;
+		f+=p;	
+	}	
+	return res;
+	
+}
+
+
+double test0() {
+	int p0 = 10;
+	int p1 = 4;
+	int p2 = 6;
+	DinkyFish * obj = new DinkyFish();
+	clock_t start = clock();
+	int my_answer = obj->monthsUntilCrowded(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 100;
+	int p1 = 4;
+	int p2 = 6;
+	DinkyFish * obj = new DinkyFish();
+	clock_t start = clock();
+	int my_answer = obj->monthsUntilCrowded(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 5;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 5;
+	int p1 = 6;
+	int p2 = 4;
+	DinkyFish * obj = new DinkyFish();
+	clock_t start = clock();
+	int my_answer = obj->monthsUntilCrowded(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 4;
+	int p1 = 6;
+	int p2 = 4;
+	DinkyFish * obj = new DinkyFish();
+	clock_t start = clock();
+	int my_answer = obj->monthsUntilCrowded(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int p0 = 1000000;
+	int p1 = 3;
+	int p2 = 2;
+	DinkyFish * obj = new DinkyFish();
+	clock_t start = clock();
+	int my_answer = obj->monthsUntilCrowded(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 19;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test5() {
+	int p0 = 431131;
+	int p1 = 764;
+	int p2 = 249;
+	DinkyFish * obj = new DinkyFish();
+	clock_t start = clock();
+	int my_answer = obj->monthsUntilCrowded(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 11;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	time = test5();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
