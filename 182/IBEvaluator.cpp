601e3811567f1890636a33661d5c24418d4f4509
shrima <shrima@shrima.(none)>
2013-03-19 11:24:30 +0530

    added 250

---
+++ 182/IBEvaluator.cpp
@@ -0,0 +1,244 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class IBEvaluator {
+public:
+	vector <int> getSummary(vector <int>, vector <int>);
+};
+
+vector <int> IBEvaluator::getSummary(vector <int> p, vector <int> a) {
+	vector<int> v;
+	for(int i=0;i<7;i++)v.push_back(0);
+	for(int i=0;i<p.size();i++){
+		v[abs(p[i]-a[i])]++;
+	}
+	int n=p.size();
+	for(int i=0;i<7;i++){
+		v[i]=(v[i]*100)/n;
+	}
+	return v;
+	
+}
+
+
+double test0() {
+	int t0[] = {1,5,7,3};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {3,5,4,5};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	IBEvaluator * obj = new IBEvaluator();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getSummary(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 25,  0,  50,  25,  0,  0,  0 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {1,1,1};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {5,6,7};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	IBEvaluator * obj = new IBEvaluator();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getSummary(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 0,  0,  0,  0,  33,  33,  33 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {3};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {3};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	IBEvaluator * obj = new IBEvaluator();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getSummary(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 100,  0,  0,  0,  0,  0,  0 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {1,5,3,5,6,4,2,5,7,6,5,2,3,4,1,4,6,5,4,7,6,6,1};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {5,1,3,2,6,4,1,7,5,2,7,4,2,6,5,7,3,1,4,6,3,1,7};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	IBEvaluator * obj = new IBEvaluator();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getSummary(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 17,  13,  21,  17,  21,  4,  4 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
