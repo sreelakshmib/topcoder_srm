59e94691aa8a9e8ce2d56dc06e39acf09067b24a
shrima <shrima@shrima.(none)>
2013-03-26 10:18:50 +0530

    added 250

---
+++ 474/PalindromesCount.cpp
@@ -0,0 +1,201 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class PalindromesCount {
+public:
+	int count(string, string);
+};
+
+int PalindromesCount::count(string A, string B) {
+	int res=0;
+	rep(i,len(A)+1){
+		string bef=A.substr(0,i);
+		string aft=A.substr(i);
+		string cur=bef+B+aft;
+		string rev=cur;
+		reverse(full(cur));
+		if(rev==cur)res++;
+	}
+	return res;
+}
+
+
+double test0() {
+	string p0 = "aba";
+	string p1 = "b";
+	PalindromesCount * obj = new PalindromesCount();
+	clock_t start = clock();
+	int my_answer = obj->count(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "aa";
+	string p1 = "a";
+	PalindromesCount * obj = new PalindromesCount();
+	clock_t start = clock();
+	int my_answer = obj->count(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "aca";
+	string p1 = "bb";
+	PalindromesCount * obj = new PalindromesCount();
+	clock_t start = clock();
+	int my_answer = obj->count(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "abba";
+	string p1 = "abba";
+	PalindromesCount * obj = new PalindromesCount();
+	clock_t start = clock();
+	int my_answer = obj->count(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "topcoder";
+	string p1 = "coder";
+	PalindromesCount * obj = new PalindromesCount();
+	clock_t start = clock();
+	int my_answer = obj->count(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
