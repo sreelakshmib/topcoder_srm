770a5c9bd59be04e533d16a7918162a586e9550c
shrima <shrima@shrima.(none)>
2013-03-27 20:11:32 +0530

    added 500

---
+++ 182/SlayingDeer.cpp
@@ -0,0 +1,205 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class SlayingDeer {
+public:
+	int getTime(int, int, int);
+};
+
+int SlayingDeer::getTime(int A, int B, int C) {
+	rep(i,500000){
+ 		if (C <= 0)
+ 		   return i;
+    	if (i % 45 < 30) 
+    		C += B;
+    	C -= A;
+    }
+    return -1;
+}
+
+
+double test0() {
+	int p0 = 5;
+	int p1 = 4;
+	int p2 = 20;
+	SlayingDeer * obj = new SlayingDeer();
+	clock_t start = clock();
+	int my_answer = obj->getTime(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 20;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 5;
+	int p1 = 4;
+	int p2 = 47;
+	SlayingDeer * obj = new SlayingDeer();
+	clock_t start = clock();
+	int my_answer = obj->getTime(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 34;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 10;
+	int p1 = 17;
+	int p2 = 1;
+	SlayingDeer * obj = new SlayingDeer();
+	clock_t start = clock();
+	int my_answer = obj->getTime(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = -1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 133;
+	int p1 = 198;
+	int p2 = 7515;
+	SlayingDeer * obj = new SlayingDeer();
+	clock_t start = clock();
+	int my_answer = obj->getTime(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 7515;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int p0 = 1;
+	int p1 = 1000;
+	int p2 = 15819;
+	SlayingDeer * obj = new SlayingDeer();
+	clock_t start = clock();
+	int my_answer = obj->getTime(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = -1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
