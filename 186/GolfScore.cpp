a5efed27a605499801ad3ea7340bd05038610bf7
shrima <shrima@shrima.(none)>
2013-03-30 17:51:56 +0530

    added 250

---
+++ 186/GolfScore.cpp
@@ -0,0 +1,201 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class GolfScore {
+public:
+	int tally(vector <int>, vector <string>);
+};
+
+int GolfScore::tally(vector <int> p, vector <string> s) {
+	int res=0;
+	for(int i=0;i<p.size();i++){
+		if(s[i]=="triple bogey")res+=p[i]+3;
+		if(s[i]=="double bogey")res+=p[i]+2;
+		if(s[i]=="bogey")res+=p[i]+1;
+		if(s[i]=="par")res+=p[i];
+		if(s[i]=="birdie")res+=p[i]-1;
+		if(s[i]=="eagle")res+=p[i]-2;
+		if(s[i]=="albatross")res+=p[i]-3;
+		if(s[i]=="hole in one")res++;
+	}
+	return res;	
+}
+
+
+double test0() {
+	int t0[] = {1, 1, 1, 1, 1, 1,
+ 1, 1, 1, 5, 5, 5,
+ 5, 5, 5, 5, 5, 5};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	string t1[] = {"bogey", "bogey", "bogey", "bogey", "bogey", "bogey",
+ "bogey", "bogey", "bogey", "eagle", "eagle", "eagle",
+ "eagle", "eagle", "eagle", "eagle", "eagle", "eagle"};
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	GolfScore * obj = new GolfScore();
+	clock_t start = clock();
+	int my_answer = obj->tally(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 45;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {3, 2, 4, 2, 2, 1,
+ 1, 1, 3, 2, 4, 4,
+ 4, 2, 3, 1, 3, 2};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	string t1[] = {"bogey", "double bogey", "par", "double bogey", "double bogey", "triple bogey",
+ "triple bogey", "triple bogey", "bogey", "double bogey", "par", "par",
+ "par", "double bogey", "bogey", "triple bogey", "bogey", "double bogey"};
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	GolfScore * obj = new GolfScore();
+	clock_t start = clock();
+	int my_answer = obj->tally(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 72;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {3, 2, 4, 2, 2, 1,
+ 1, 1, 3, 2, 4, 4,
+ 4, 2, 3, 1, 3, 2};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	string t1[] = {"eagle", "birdie", "albatross", "birdie", "birdie", "par",
+ "hole in one", "par", "eagle", "birdie", "albatross", "albatross",
+ "albatross", "birdie", "eagle", "hole in one", "eagle", "birdie"};
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	GolfScore * obj = new GolfScore();
+	clock_t start = clock();
+	int my_answer = obj->tally(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 18;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {4, 1, 3, 3, 4, 4,
+ 1, 4, 2, 3, 3, 5,
+ 4, 1, 4, 4, 2, 1};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	string t1[] = {"triple bogey", "triple bogey", "bogey", "par", "triple bogey", "double bogey",
+ "triple bogey", "triple bogey", "par", "eagle", "bogey", "bogey",
+ "birdie", "par", "triple bogey", "eagle", "triple bogey", "triple bogey"};
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	GolfScore * obj = new GolfScore();
+	clock_t start = clock();
+	int my_answer = obj->tally(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 77;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
