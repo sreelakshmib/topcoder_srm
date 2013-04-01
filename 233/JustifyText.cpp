84cc83bb23edb2377a7733a92371a98e2e0edaf8
shrima <shrima@shrima.(none)>
2013-04-01 09:16:32 +0530

    added 250

---
+++ 233/JustifyText.cpp
@@ -0,0 +1,201 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class JustifyText {
+public:
+	vector <string> format(vector <string>);
+};
+
+vector <string> JustifyText::format(vector <string> text) {
+	int longleng=0;
+	rep(i,sz(text)){
+		if(longleng<len(text[i]))
+			longleng=len(text[i]);
+	}
+	rep(i,sz(text)){
+		string space="";
+		rep(j,(longleng-len(text[i])))
+			space+=" ";
+		text[i]=space+text[i];
+	}
+	return text;
+}
+
+
+double test0() {
+	string t0[] = {"BOB","TOMMY","JIM"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	JustifyText * obj = new JustifyText();
+	clock_t start = clock();
+	vector <string> my_answer = obj->format(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t1[] = {"  BOB", "TOMMY", "  JIM" };
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<"\""<<p1[0]<<"\"";
+		for (int i=1; i<p1.size(); i++)
+			cout <<", \"" <<p1[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"JOHN","JAKE","ALAN","BLUE"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	JustifyText * obj = new JustifyText();
+	clock_t start = clock();
+	vector <string> my_answer = obj->format(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t1[] = {"JOHN", "JAKE", "ALAN", "BLUE" };
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<"\""<<p1[0]<<"\"";
+		for (int i=1; i<p1.size(); i++)
+			cout <<", \"" <<p1[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string t0[] = {"LONGEST","A","LONGER","SHORT"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	JustifyText * obj = new JustifyText();
+	clock_t start = clock();
+	vector <string> my_answer = obj->format(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t1[] = {"LONGEST", "      A", " LONGER", "  SHORT" };
+	vector <string> p1(t1, t1+sizeof(t1)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<"\""<<p1[0]<<"\"";
+		for (int i=1; i<p1.size(); i++)
+			cout <<", \"" <<p1[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
