6dc35f084cb18a59a9795439468d3b14ae877521
shrima <shrima@shrima.(none)>
2013-04-01 08:35:38 +0530

    added 250

---
+++ 198/Reppity.java
@@ -0,0 +1,121 @@
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+import java.awt.geom.*;
+
+public class Reppity
+{
+	public int longestRep(String input){
+	int len=0;
+	for(int i=0;i<input.length();i++){
+		for(int j=i+1;j<input.length();j++){
+			String sub=input.substring(i,j);
+			int f1=input.indexOf(sub);
+			int x=sub.length();
+			int f2=input.indexOf(sub,f1+x);
+			if(f2>-1) 
+				len=Math.max(len,x);
+		}
+	}
+	return len;
+	}
+	
+	public static void main(String[] args)
+	{
+		long time;
+		int answer;
+		boolean errors = false;
+		int desiredAnswer;
+		
+		
+		time = System.currentTimeMillis();
+		answer = new Reppity().longestRep("ABCDEXXXYYYZZZABCDEZZZYYYXXX");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 5;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new Reppity().longestRep("abcdabcdabcdabCD");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 6;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new Reppity().longestRep("abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 25;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new Reppity().longestRep("againANDagainANDagainANDagainANDagainANDagain");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 21;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new Reppity().longestRep("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX");
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 0;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		
+		
+		if (errors)
+			System.out.println("Some of the test cases had errors :-(");
+		else
+			System.out.println("You're a stud (at least on the test data)! :-D ");
+	}
+
+}
+//Powered by [KawigiEdit] 2.0!
