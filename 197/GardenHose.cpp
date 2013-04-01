d730afb323662cc6a0ce76286d53ceb71509ceaa
shrima <shrima@shrima.(none)>
2013-04-01 08:35:16 +0530

    added 250

---
+++ 197/GardenHose.cpp
@@ -0,0 +1,211 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class GardenHose {
+public:
+	int countDead(int, int, int, int);
+};
+
+
+int GardenHose::countDead(int n, int row, int col, int h) {
+	int cnt=0;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			int c=min(min((i+1)*row,(j+1)*col),min((n-i)*row,(n-j)*col));
+			cnt+=c>h;
+		}
+	}
+	return cnt;
+}
+
+
+double test0() {
+	int p0 = 3;
+	int p1 = 2;
+	int p2 = 1;
+	int p3 = 2;
+	GardenHose * obj = new GardenHose();
+	clock_t start = clock();
+	int my_answer = obj->countDead(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 3;
+	int p1 = 2;
+	int p2 = 1;
+	int p3 = 1;
+	GardenHose * obj = new GardenHose();
+	clock_t start = clock();
+	int my_answer = obj->countDead(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 6;
+	int p1 = 2;
+	int p2 = 5;
+	int p3 = 5;
+	GardenHose * obj = new GardenHose();
+	clock_t start = clock();
+	int my_answer = obj->countDead(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 8;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 6;
+	int p1 = 2;
+	int p2 = 5;
+	int p3 = 3;
+	GardenHose * obj = new GardenHose();
+	clock_t start = clock();
+	int my_answer = obj->countDead(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 24;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int p0 = 50;
+	int p1 = 50;
+	int p2 = 50;
+	int p3 = 49;
+	GardenHose * obj = new GardenHose();
+	clock_t start = clock();
+	int my_answer = obj->countDead(p0, p1, p2, p3);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p4 = 2500;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p4 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p4 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
