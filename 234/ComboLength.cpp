de4e254e73f27b3b5a76b4cc4b63352952210006
shrima <shrima@shrima.(none)>
2013-04-01 09:23:07 +0530

    added 250

---
+++ 234/ComboLength.cpp
@@ -0,0 +1,200 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class ComboLength {
+public:
+	int howLong(string);
+};
+
+int ComboLength::howLong(string moves) {
+	int res=1,curr=1;
+	char prev=moves[0];
+	repx(i,1,len(moves)){
+		if(prev!=moves[i]){
+			if(curr>res){
+				res=curr;
+			}
+			curr=1;
+			prev=moves[i];
+		}else
+			curr++;
+	}
+	return max(curr,res);
+}
+
+
+double test0() {
+	string p0 = "AAA";
+	ComboLength * obj = new ComboLength();
+	clock_t start = clock();
+	int my_answer = obj->howLong(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "AAABBBBAA";
+	ComboLength * obj = new ComboLength();
+	clock_t start = clock();
+	int my_answer = obj->howLong(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "BBAABBAABBAABBAA";
+	ComboLength * obj = new ComboLength();
+	clock_t start = clock();
+	int my_answer = obj->howLong(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+	ComboLength * obj = new ComboLength();
+	clock_t start = clock();
+	int my_answer = obj->howLong(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 50;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "AAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAA";
+	ComboLength * obj = new ComboLength();
+	clock_t start = clock();
+	int my_answer = obj->howLong(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 25;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
