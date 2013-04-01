9f5551956bd91fafe2beac565714e68866e0dbea
shrima <shrima@shrima.(none)>
2013-04-01 09:35:21 +0530

    added 250

---
+++ 236/MassiveNumbers.cpp
@@ -0,0 +1,181 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class MassiveNumbers {
+public:
+	string getLargest(string, string);
+};
+
+string MassiveNumbers::getLargest(string numberA, string numberB) {
+	string nA=numberA;
+	string nB=numberB;
+	int p=nA.find('^');
+	nA[p]=' ';
+	p=nB.find('^');
+	nB[p]=' ';
+	stringstream s1(nA),s2(nB);
+	int a,b,c,d;
+	s1>>a>>b;
+	s2>>c>>d;
+	int m=b*log(a);
+	int n=d*log(c);
+	if(m>n)
+		return numberA;
+	else
+		return numberB;
+}
+
+
+double test0() {
+	string p0 = "3^100";
+	string p1 = "2^150";
+	MassiveNumbers * obj = new MassiveNumbers();
+	clock_t start = clock();
+	string my_answer = obj->getLargest(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "3^100";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "1^1000";
+	string p1 = "2^1";
+	MassiveNumbers * obj = new MassiveNumbers();
+	clock_t start = clock();
+	string my_answer = obj->getLargest(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "2^1";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "893^605";
+	string p1 = "396^906";
+	MassiveNumbers * obj = new MassiveNumbers();
+	clock_t start = clock();
+	string my_answer = obj->getLargest(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "396^906";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "999^1000";
+	string p1 = "1000^999";
+	MassiveNumbers * obj = new MassiveNumbers();
+	clock_t start = clock();
+	string my_answer = obj->getLargest(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "999^1000";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
