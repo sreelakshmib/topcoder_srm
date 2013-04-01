2c0545113309679e0b1dcaefcf43e927d2359b7a
shrima <shrima@shrima.(none)>
2013-04-01 18:37:07 +0530

    added 250

---
+++ 241/BlackjackWinner.cpp
@@ -0,0 +1,191 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class BlackjackWinner {
+public:
+	int winnings(int, int, int, int, int);
+};
+
+int BlackjackWinner::winnings(int bet, int dealer, int dealerBlackjack, int player, int blackjack) {
+	if(dealerBlackjack){
+		if(blackjack)
+			return 0;
+		return -bet;
+	}
+	if(blackjack)
+		return 3*bet/2;
+	if(player<22){
+		if(player==dealer)
+			return 0;
+		if(player>dealer || dealer>21)
+			return bet;
+	}
+	return -bet;
+}
+
+
+double test0() {
+	int p0 = 10;
+	int p1 = 20;
+	int p2 = 0;
+	int p3 = 21;
+	int p4 = 0;
+	BlackjackWinner * obj = new BlackjackWinner();
+	clock_t start = clock();
+	int my_answer = obj->winnings(p0, p1, p2, p3, p4);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p5 = 10;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p5 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p5 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 26;
+	int p1 = 21;
+	int p2 = 1;
+	int p3 = 21;
+	int p4 = 0;
+	BlackjackWinner * obj = new BlackjackWinner();
+	clock_t start = clock();
+	int my_answer = obj->winnings(p0, p1, p2, p3, p4);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p5 = -26;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p5 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p5 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 100;
+	int p1 = 25;
+	int p2 = 0;
+	int p3 = 21;
+	int p4 = 1;
+	BlackjackWinner * obj = new BlackjackWinner();
+	clock_t start = clock();
+	int my_answer = obj->winnings(p0, p1, p2, p3, p4);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p5 = 150;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p5 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p5 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 78;
+	int p1 = 22;
+	int p2 = 0;
+	int p3 = 23;
+	int p4 = 0;
+	BlackjackWinner * obj = new BlackjackWinner();
+	clock_t start = clock();
+	int my_answer = obj->winnings(p0, p1, p2, p3, p4);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p5 = -78;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p5 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p5 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
