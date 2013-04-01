9ddeb291b55bb63ae2f0d6446324e4458a5a6574
shrima <shrima@shrima.(none)>
2013-04-01 09:13:04 +0530

    added 250

---
+++ 232/BritishCoins.cpp
@@ -0,0 +1,290 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class BritishCoins {
+public:
+	vector <int> coins(int);
+};
+
+vector <int> BritishCoins::coins(int pence) {
+	VI res;
+	res.pb(pence/(12*20));
+	pence%=(12*20);
+	res.pb(pence/12);
+	pence%=12;
+	res.pb(pence);
+	return res;
+	
+}
+
+
+double test0() {
+	int p0 = 533;
+	BritishCoins * obj = new BritishCoins();
+	clock_t start = clock();
+	vector <int> my_answer = obj->coins(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 2,
+  4,
+  5 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 0;
+	BritishCoins * obj = new BritishCoins();
+	clock_t start = clock();
+	vector <int> my_answer = obj->coins(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 0,
+  0,
+  0 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 6;
+	BritishCoins * obj = new BritishCoins();
+	clock_t start = clock();
+	vector <int> my_answer = obj->coins(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 0,
+  0,
+  6 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 4091;
+	BritishCoins * obj = new BritishCoins();
+	clock_t start = clock();
+	vector <int> my_answer = obj->coins(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 17,
+  0,
+  11 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int p0 = 10000;
+	BritishCoins * obj = new BritishCoins();
+	clock_t start = clock();
+	vector <int> my_answer = obj->coins(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t1[] = { 41,
+  13,
+  4 };
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p1.size() > 0) {
+		cout <<p1[0];
+		for (int i=1; i<p1.size(); i++)
+			cout <<", " <<p1[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p1) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
