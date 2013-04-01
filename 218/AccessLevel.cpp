1ce1b278c7fb36c362deacc7aa18448e334f36d7
shrima <shrima@shrima.(none)>
2013-04-01 08:56:24 +0530

    added 500

---
+++ 218/AccessLevel.cpp
@@ -0,0 +1,161 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class AccessLevel {
+public:
+	string canAccess(vector <int>, int);
+};
+
+string AccessLevel::canAccess(vector <int> rights, int min) {
+	string res="";
+	for(int i=0;i<rights.size();i++){
+		if(rights[i]<min)
+			res+='D';
+		else 
+			res+='A';
+	}
+	return res;
+}
+
+
+double test0() {
+	int t0[] = {0,1,2,3,4,5};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 2;
+	AccessLevel * obj = new AccessLevel();
+	clock_t start = clock();
+	string my_answer = obj->canAccess(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "DDAAAA";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {5,3,2,10,0};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 20;
+	AccessLevel * obj = new AccessLevel();
+	clock_t start = clock();
+	string my_answer = obj->canAccess(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "DDDDD";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	vector <int> p0;
+	int p1 = 20;
+	AccessLevel * obj = new AccessLevel();
+	clock_t start = clock();
+	string my_answer = obj->canAccess(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {34,78,9,52,11,1};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 49;
+	AccessLevel * obj = new AccessLevel();
+	clock_t start = clock();
+	string my_answer = obj->canAccess(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p2 = "DADADD";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p2 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
---
+++ 218/FolderSize.cpp
@@ -0,0 +1,242 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class FolderSize {
+public:
+	vector <int> calculateWaste(vector <string>, int, int);
+};
+
+int calc(int a,int b){
+	if(a%b==0)return 0;
+	return b- a%b;
+}
+
+vector <int> FolderSize::calculateWaste(vector <string> files, int fc, int c) {
+	vector<int> res(fc,0);
+	for(int i=0;i<files.size();i++){
+		stringstream ss(files[i]);
+		int a,b;
+		ss>>a>>b;
+		res[a]+=calc(b,c);
+		
+	}
+	return res;
+}
+
+
+double test0() {
+	string t0[] = {"0 55", "0 47", "1 86"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 3;
+	int p2 = 50;
+	FolderSize * obj = new FolderSize();
+	clock_t start = clock();
+	vector <int> my_answer = obj->calculateWaste(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t3[] = { 48,  14,  0 };
+	vector <int> p3(t3, t3+sizeof(t3)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p3.size() > 0) {
+		cout <<p3[0];
+		for (int i=1; i<p3.size(); i++)
+			cout <<", " <<p3[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p3) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"0 123", "2 456", "4 789", "6 012", "8 345"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 10;
+	int p2 = 98;
+	FolderSize * obj = new FolderSize();
+	clock_t start = clock();
+	vector <int> my_answer = obj->calculateWaste(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t3[] = { 73,  0,  34,  0,  93,  0,  86,  0,  47,  0 };
+	vector <int> p3(t3, t3+sizeof(t3)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p3.size() > 0) {
+		cout <<p3[0];
+		for (int i=1; i<p3.size(); i++)
+			cout <<", " <<p3[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p3) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	vector <string> p0;
+	int p1 = 5;
+	int p2 = 100;
+	FolderSize * obj = new FolderSize();
+	clock_t start = clock();
+	vector <int> my_answer = obj->calculateWaste(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t3[] = { 0,  0,  0,  0,  0 };
+	vector <int> p3(t3, t3+sizeof(t3)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p3.size() > 0) {
+		cout <<p3[0];
+		for (int i=1; i<p3.size(); i++)
+			cout <<", " <<p3[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p3) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string t0[] = {"0 93842", "1 493784", "2 43212", "3 99327", "4 456209",
+"5 947243", "6 59348", "7 58237", "8 5834", "9 492384",
+"0 58342", "3 538432", "6 1432", "9 453983", "2 4321",
+"4 583729", "6 6974", "8 9864", "4 43211", "8 38437"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 10;
+	int p2 = 22485;
+	FolderSize * obj = new FolderSize();
+	clock_t start = clock();
+	vector <int> my_answer = obj->calculateWaste(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t3[] = { 27696,  886,  19922,  14306,  18616,  19612,  44671,  9218,  35805,  20488 };
+	vector <int> p3(t3, t3+sizeof(t3)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p3.size() > 0) {
+		cout <<p3[0];
+		for (int i=1; i<p3.size(); i++)
+			cout <<", " <<p3[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p3) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
