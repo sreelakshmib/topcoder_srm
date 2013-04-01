ba06f0b9a351b21c1a0ec05c8b7711a3ca4bd149
shrima <shrima@shrima.(none)>
2013-04-01 08:53:26 +0530

    added 250

---
+++ 209/MovingAverages.cpp
@@ -0,0 +1,211 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class MovingAverages {
+public:
+	vector <int> calculate(vector <string>, int);
+};
+
+vector <int> MovingAverages::calculate(vector <string> times, int n) {
+	vector<int>res;
+	int prev=0;
+	int a=0;
+	for(int i=0;i<times.size()-n+1;i++){
+	a=0;
+		for(int j=0;j<n;j++){
+		stringstream ss;
+		int p=times[i+j].find(':');times[i+j][p]=' ';
+		p=times[i+j].find(':');times[i+j][p]=' ';
+		ss<<times[i+j];
+		int h,m,s;
+		ss>>h>>m>>s;
+		a+=h*3600+m*60+s;
+	}
+	res.push_back(a/n);
+	}
+	
+	return res;
+	
+}
+
+
+double test0() {
+	string t0[] = {"01:19:10", "01:17:40", "01:19:44", "01:17:23", "01:17:07"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 3;
+	MovingAverages * obj = new MovingAverages();
+	clock_t start = clock();
+	vector <int> my_answer = obj->calculate(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 4731,  4695,  4684 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"01:19:10", "01:17:40", "01:19:44", "01:17:23", "01:17:07"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 1;
+	MovingAverages * obj = new MovingAverages();
+	clock_t start = clock();
+	vector <int> my_answer = obj->calculate(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 4750,  4660,  4784,  4643,  4627 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string t0[] = {"01:19:10", "01:17:40", "01:19:44", "01:17:23", "01:17:07"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 5;
+	MovingAverages * obj = new MovingAverages();
+	clock_t start = clock();
+	vector <int> my_answer = obj->calculate(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 4692 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
