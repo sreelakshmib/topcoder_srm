dd04a6a2acf4c175688c983342c0e22d619abb82
shrima <shrima@shrima.(none)>
2013-04-01 08:57:12 +0530

    added 250

---
+++ 224/InsideOut.cpp
@@ -0,0 +1,178 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class InsideOut {
+public:
+	string unscramble(string);
+};
+
+string InsideOut::unscramble(string line) {
+	string first=line.substr(0,line.length()/2);
+	string second=line.substr(line.length()/2);
+	reverse(first.begin(),first.end());
+	reverse(second.begin(),second.end());
+	cout<<first<<second<<endl;
+	return first+second;
+}
+
+
+double test0() {
+	string p0 = "I ENIL SIHTHSIREBBIG S";
+	InsideOut * obj = new InsideOut();
+	clock_t start = clock();
+	string my_answer = obj->unscramble(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "THIS LINE IS GIBBERISH";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "LEVELKAYAK";
+	InsideOut * obj = new InsideOut();
+	clock_t start = clock();
+	string my_answer = obj->unscramble(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "LEVELKAYAK";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "H YPPAHSYADILO";
+	InsideOut * obj = new InsideOut();
+	clock_t start = clock();
+	string my_answer = obj->unscramble(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "HAPPY HOLIDAYS";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	InsideOut * obj = new InsideOut();
+	clock_t start = clock();
+	string my_answer = obj->unscramble(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "MLKJIHGFEDCBAZYXWVUTSRQPON";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "RUT OWT SNEH HCNERF EERHTEGDIRTRAP A DNA  SEVODELT";
+	InsideOut * obj = new InsideOut();
+	clock_t start = clock();
+	string my_answer = obj->unscramble(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p1 = "THREE FRENCH HENS TWO TURTLEDOVES  AND A PARTRIDGE";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p1 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
