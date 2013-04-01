51157a6b9c81e53dcdc3b47d0e62ca3182775da0
shrima <shrima@shrima.(none)>
2013-04-01 08:56:36 +0530

    added 250

---
+++ 219/WaiterTipping.cpp
@@ -0,0 +1,215 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class WaiterTipping {
+public:
+	int maxPercent(int, int, int);
+};
+
+int WaiterTipping::maxPercent(int total, int taxPercent, int money) {
+	for(int tip=1000000;tip>=0;tip--){
+		if(total+ (total*taxPercent/100) + (total*tip/100) <=money)
+			return tip;
+	}
+	return -1;
+}
+
+
+double test0() {
+	int p0 = 500;
+	int p1 = 10;
+	int p2 = 600;
+	WaiterTipping * obj = new WaiterTipping();
+	clock_t start = clock();
+	int my_answer = obj->maxPercent(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 10;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 500;
+	int p1 = 10;
+	int p2 = 604;
+	WaiterTipping * obj = new WaiterTipping();
+	clock_t start = clock();
+	int my_answer = obj->maxPercent(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 10;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 850;
+	int p1 = 8;
+	int p2 = 870;
+	WaiterTipping * obj = new WaiterTipping();
+	clock_t start = clock();
+	int my_answer = obj->maxPercent(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = -1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 226;
+	int p1 = 48;
+	int p2 = 584;
+	WaiterTipping * obj = new WaiterTipping();
+	clock_t start = clock();
+	int my_answer = obj->maxPercent(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 111;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int p0 = 123;
+	int p1 = 52;
+	int p2 = 696;
+	WaiterTipping * obj = new WaiterTipping();
+	clock_t start = clock();
+	int my_answer = obj->maxPercent(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 415;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test5() {
+	int p0 = 500;
+	int p1 = 10;
+	int p2 = 550;
+	WaiterTipping * obj = new WaiterTipping();
+	clock_t start = clock();
+	int my_answer = obj->maxPercent(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	time = test5();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
