c5a8498a8929031ac655602e51f22c4ad6ad405e
shrima <shrima@shrima.(none)>
2013-04-01 08:51:11 +0530

    added 250

---
+++ 211/grafixClick.cpp
@@ -0,0 +1,286 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class grafixClick {
+public:
+	vector <int> checkSaveButton(vector <int>, vector <int>);
+};
+
+vector <int> grafixClick::checkSaveButton(vector <int> mouseRows, vector <int> mouseCols) {
+	vector<int> res;
+	for(int i=0;i<mouseRows.size();i++){
+		if(mouseRows[i]>=20&& mouseRows[i]<=39 &&
+			mouseCols[i]>=50 && mouseCols[i]<=99)
+				res.push_back(i);
+	}
+	return res;
+}
+
+
+double test0() {
+	int t0[] = {20, 39, 100};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {99, 50, 200};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	grafixClick * obj = new grafixClick();
+	clock_t start = clock();
+	vector <int> my_answer = obj->checkSaveButton(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 0,  1 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {0, 100, 399};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {0, 200, 599};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	grafixClick * obj = new grafixClick();
+	clock_t start = clock();
+	vector <int> my_answer = obj->checkSaveButton(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	vector <int> p2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {30};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {75};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	grafixClick * obj = new grafixClick();
+	clock_t start = clock();
+	vector <int> my_answer = obj->checkSaveButton(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 0 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {10, 20, 30, 30, 30, 30, 34, 35, 345};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {10, 20, 30, 50, 60, 80, 34, 35, 345};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	grafixClick * obj = new grafixClick();
+	clock_t start = clock();
+	vector <int> my_answer = obj->checkSaveButton(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 3,  4,  5 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int t0[] = {57, 28, 18, 25, 36, 12, 33, 44, 13, 32,
+ 32, 51, 11, 27, 8, 51, 17, 34, 10, 16,
+ 47, 57, 20, 57, 32, 14, 13, 37, 10, 16,
+ 49, 37, 52, 8, 18, 44, 50, 43, 11, 1,
+ 21, 22, 17, 35, 28, 53, 56, 16, 11, 44};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int t1[] = {146, 22, 41, 88, 123, 99, 43, 110, 25, 64,
+ 141, 110, 70, 34, 99, 103, 60, 64, 142, 109,
+ 133, 144, 72, 68, 25, 32, 21, 140, 30, 105,
+ 32, 72, 114, 97, 35, 131, 103, 110, 133, 81,
+ 125, 36, 76, 78, 77, 47, 50, 94, 22, 20};
+	vector <int> p1(t1, t1+sizeof(t1)/sizeof(int));
+	grafixClick * obj = new grafixClick();
+	clock_t start = clock();
+	vector <int> my_answer = obj->checkSaveButton(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = { 3,  9,  17,  22,  31,  43,  44 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
