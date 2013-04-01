36f22400809f8e722618263195ed69c256c618f9
shrima <shrima@shrima.(none)>
2013-04-01 08:46:54 +0530

    added 250

---
+++ 206/Bits.cpp
@@ -0,0 +1,170 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class Bits {
+public:
+	int minBits(int);
+};
+
+
+int Bits::minBits(int n) {
+	int res=2;
+	if(n<=1)return 1;
+	for(int i=2;;i++){
+		res*=2;
+		if(res>n)
+		return i;
+	}
+	return 0;
+}
+
+
+double test0() {
+	int p0 = 32;
+	Bits * obj = new Bits();
+	clock_t start = clock();
+	int my_answer = obj->minBits(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 6;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 12;
+	Bits * obj = new Bits();
+	clock_t start = clock();
+	int my_answer = obj->minBits(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 1;
+	Bits * obj = new Bits();
+	clock_t start = clock();
+	int my_answer = obj->minBits(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 1500;
+	Bits * obj = new Bits();
+	clock_t start = clock();
+	int my_answer = obj->minBits(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 11;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
