66bcc034ffbfacbd758a17ab2804992f906d4503
shrima <shrima@shrima.(none)>
2013-04-01 08:41:34 +0530

    added 250

---
+++ 201/Multiples.cpp
@@ -0,0 +1,176 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class Multiples {
+public:
+	int number(int, int, int);
+};
+
+int Multiples::number(int min, int max, int factor) {
+	int res=0;
+	while(min%factor!=0)min++;
+	while(max%factor!=0)max--;
+	for(int i=min;i<=max;i+=factor){
+		res++;
+	}
+	return res;
+}
+
+
+double test0() {
+	int p0 = 0;
+	int p1 = 14;
+	int p2 = 5;
+	Multiples * obj = new Multiples();
+	clock_t start = clock();
+	int my_answer = obj->number(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 7;
+	int p1 = 24;
+	int p2 = 3;
+	Multiples * obj = new Multiples();
+	clock_t start = clock();
+	int my_answer = obj->number(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 6;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = -123456;
+	int p1 = 654321;
+	int p2 = 997;
+	Multiples * obj = new Multiples();
+	clock_t start = clock();
+	int my_answer = obj->number(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 780;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = -75312;
+	int p1 = 407891;
+	int p2 = 14;
+	Multiples * obj = new Multiples();
+	clock_t start = clock();
+	int my_answer = obj->number(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p3 = 34515;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p3 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
