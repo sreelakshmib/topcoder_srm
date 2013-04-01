db09bd1ca7ed9cc8025f2c609881118dece2ecee
shrima <shrima@shrima.(none)>
2013-04-01 19:21:05 +0530

    added 250

---
+++ 247/TriangleType.cpp
@@ -0,0 +1,180 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class TriangleType {
+public:
+	string type(int, int, int);
+};
+
+string TriangleType::type(int a, int b, int c) {
+	VI res;
+	res.pb(a);res.pb(b);res.pb(c);
+	sort(full(res));
+	if(res[0]+res[1]<=res[2])
+		return "IMPOSSIBLE";
+	if(res[0]*res[0]+res[1]*res[1]==res[2]*res[2])
+		return "RIGHT";
+	if(res[0]*res[0]+res[1]*res[1]>res[2]*res[2])
+		return "ACUTE";
+	if(res[0]*res[0]+res[1]*res[1]<res[2]*res[2])
+		return "OBTUSE";
+}
+
+
+double test0() {
+	int p0 = 3;
+	int p1 = 4;
+	int p2 = 5;
+	TriangleType * obj = new TriangleType();
+	clock_t start = clock();
+	string my_answer = obj->type(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "RIGHT";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 3;
+	int p1 = 4;
+	int p2 = 4;
+	TriangleType * obj = new TriangleType();
+	clock_t start = clock();
+	string my_answer = obj->type(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "ACUTE";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 3;
+	int p1 = 4;
+	int p2 = 6;
+	TriangleType * obj = new TriangleType();
+	clock_t start = clock();
+	string my_answer = obj->type(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "OBTUSE";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 7;
+	int p1 = 4;
+	int p2 = 3;
+	TriangleType * obj = new TriangleType();
+	clock_t start = clock();
+	string my_answer = obj->type(p0, p1, p2);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string p3 = "IMPOSSIBLE";
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t\"" << p3 <<"\"" <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t\"" << my_answer<<"\"" <<endl;
+	if (p3 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
