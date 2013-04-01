8228d436890a40eb2e6c992a22993f3c857a17df
shrima <shrima@shrima.(none)>
2013-04-01 08:43:23 +0530

    added 250

---
+++ 202/LetterStrings.cpp
@@ -0,0 +1,172 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class LetterStrings {
+public:
+	int sum(vector <string>);
+};
+
+int LetterStrings::sum(vector <string> s) {
+	int cnt=0;
+	for(int i=0;i<s.size();i++){
+		for(int j=0;j<s[i].length();j++){
+			if(s[i][j]!='-')cnt++;
+		}
+	}
+	return cnt;
+}
+
+
+double test0() {
+	string t0[] = {"-"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	LetterStrings * obj = new LetterStrings();
+	clock_t start = clock();
+	int my_answer = obj->sum(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"A"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	LetterStrings * obj = new LetterStrings();
+	clock_t start = clock();
+	int my_answer = obj->sum(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 1;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string t0[] = {"-----Abc"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	LetterStrings * obj = new LetterStrings();
+	clock_t start = clock();
+	int my_answer = obj->sum(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string t0[] = {"-A-B-C-D", "--------EFGHI", "JKLMNOPQR", "---STU-VW-XYZ"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	LetterStrings * obj = new LetterStrings();
+	clock_t start = clock();
+	int my_answer = obj->sum(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 26;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
