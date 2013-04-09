ea44c31ac8cd0ed0085c656817f11926410da74a
shrima <shrima@shrima.(none)>
2013-04-09 10:39:31 +0530

    added 500

---
+++ 186/WordWrap.cpp
@@ -0,0 +1,277 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class WordWrap {
+public:
+	vector <string> justify(vector <string>, int);
+};
+
+vector <string> WordWrap::justify(vector <string> lines, int colW) {
+	VS res;
+	string common="";
+	rep(i,sz(lines)){
+		common+=lines[i];
+		common+=" ";
+	}
+	stringstream ss(common);
+	string cur,now="";
+	ss>>now;
+	while(ss>>cur){
+		if(len(now)+len(cur)<colW){
+			now+=" "+cur;
+		}	
+		else{
+			res.pb(now);
+			now=cur;
+		}
+	}
+	if(len(now))
+		res.pb(now);
+	return res;
+}
+
+
+double test0() {
+	string t0[] = {"now is the time for all good men",
+ "to come to the aid of their country"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 20;
+	WordWrap * obj = new WordWrap();
+	clock_t start = clock();
+	vector <string> my_answer = obj->justify(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t2[] = { "now is the time for",  "all good men to come",  "to the aid of their",  "country" };
+	vector <string> p2(t2, t2+sizeof(t2)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<"\""<<p2[0]<<"\"";
+		for (int i=1; i<p2.size(); i++)
+			cout <<", \"" <<p2[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"now",
+ "is the time for",
+ "all",
+ "good",
+ "men",
+ "to",
+ "come to the aid",
+ "of",
+ "their",
+ "country"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 20;
+	WordWrap * obj = new WordWrap();
+	clock_t start = clock();
+	vector <string> my_answer = obj->justify(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t2[] = { "now is the time for",  "all good men to come",  "to the aid of their",  "country" };
+	vector <string> p2(t2, t2+sizeof(t2)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<"\""<<p2[0]<<"\"";
+		for (int i=1; i<p2.size(); i++)
+			cout <<", \"" <<p2[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string t0[] = {"four score and seven years ago", 
+ "our fathers brought forth",
+ "upon this continent",
+ "a new nation",
+ "conceived in liberty", 
+ "and dedicated to the proposition", 
+ "that all men are created",
+ "equal"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 50;
+	WordWrap * obj = new WordWrap();
+	clock_t start = clock();
+	vector <string> my_answer = obj->justify(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t2[] = { "four score and seven years ago our fathers brought",  "forth upon this continent a new nation conceived",  "in liberty and dedicated to the proposition that",  "all men are created equal" };
+	vector <string> p2(t2, t2+sizeof(t2)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<"\""<<p2[0]<<"\"";
+		for (int i=1; i<p2.size(); i++)
+			cout <<", \"" <<p2[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string t0[] = {"th e ow l an d th e pu ss yc at",
+ "we nt to se a",
+ "on a be au ti fu l pe a gr ee n bo at"};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 4;
+	WordWrap * obj = new WordWrap();
+	clock_t start = clock();
+	vector <string> my_answer = obj->justify(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	string t2[] = { "th e",  "ow l",  "an d",  "th e",  "pu",  "ss",  "yc",  "at",  "we",  "nt",  "to",  "se a",  "on a",  "be",  "au",  "ti",  "fu l",  "pe a",  "gr",  "ee n",  "bo",  "at" };
+	vector <string> p2(t2, t2+sizeof(t2)/sizeof(string));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<"\""<<p2[0]<<"\"";
+		for (int i=1; i<p2.size(); i++)
+			cout <<", \"" <<p2[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<"\""<<my_answer[0]<<"\"";
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", \"" <<my_answer[i]<<"\"";
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
