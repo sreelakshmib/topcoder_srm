9ec86dca5b1e18408c40762c60fa5d530881c114
shrima <shrima@shrima.(none)>
2013-04-09 18:17:57 +0530

    added 250

---
+++ 268/CrossWordPuzzle.cpp
@@ -0,0 +1,242 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iterator>
+
+#define pb push_back
+#define VI vector<int>
+#define VS vector<string>
+#define sz(v) v.size()
+#define len(s) s.length()
+#define full(v) v.begin(),v.end()
+
+#define repx(i,x,n) for(int i=x;i<n;i++)
+#define rep(i,n) repx(i,0,n)
+
+typedef long long ll;
+
+const int INF = 1<<30;
+
+using namespace std;
+
+class CrossWordPuzzle {
+public:
+	int countWords(vector <string>, int);
+};
+
+int CrossWordPuzzle::countWords(vector <string> board, int size) {
+	string p="X";
+	rep(i,size)p+='.';
+	p+='X';
+	rep(i,sz(board)){
+		board[i]='X'+board[i]+'X';
+	}
+	int res=0;
+	rep(i,sz(board)){
+		while(board[i].find(p)<len(board[i])){
+			res++;
+			int pos=board[i].find(p);
+			repx(j,pos,pos+len(p))
+				board[i][j]='X';
+		}
+	}
+	return res;
+}
+
+
+double test0() {
+	string t0[] = {"X....X",
+ "X.XX.X",
+ "...X..",
+ "X.XX.X",
+ "..X..."};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 3;
+	CrossWordPuzzle * obj = new CrossWordPuzzle();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string t0[] = {"...X...",
+ ".X...X.",
+ "..X.X..",
+ "X..X..X",
+ "..X.X..",
+ ".X...X.",
+ "...X..."};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 3;
+	CrossWordPuzzle * obj = new CrossWordPuzzle();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 6;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string t0[] = {".....X....X....",
+ ".....X....X....",
+ "..........X....",
+ "....X....X.....",
+ "...X....X....XX",
+ "XXX...X....X...",
+ ".....X....X....",
+ ".......X.......",
+ "....X....X.....",
+ "...X....X...XXX",
+ "XX....X....X...",
+ ".....X....X....",
+ "....X..........",
+ "....X....X.....",
+ "....X....X....."}
+;
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 5;
+	CrossWordPuzzle * obj = new CrossWordPuzzle();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 8;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string t0[] = {"...",
+ "...",
+ "..."};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 50;
+	CrossWordPuzzle * obj = new CrossWordPuzzle();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string t0[] = {"....",
+ "....",
+ "...."};
+	vector <string> p0(t0, t0+sizeof(t0)/sizeof(string));
+	int p1 = 3;
+	CrossWordPuzzle * obj = new CrossWordPuzzle();
+	clock_t start = clock();
+	int my_answer = obj->countWords(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
